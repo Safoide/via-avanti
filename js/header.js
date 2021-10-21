76ce7fce1a5d05b11e8c24b23baaf3986e19a9c2
Safoide <romanoibarra12@gmail.com>
2021-10-21 19:02:42 -0300

    Agregado de menu hamburguesa

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
 const dropdownEl = document.getElementById('mydropdown');
+const burguerEl = document.getElementById('burguerToggle');
 
 dropdownEl.addEventListener('click', toggleActive);
 document.addEventListener('click', documentClick);
+burguerEl.addEventListener('click', burguerToggleActive);
 
 function toggleActive() {
     dropdownEl.classList.toggle('active');
@@ -10,4 +12,11 @@ function toggleActive() {
 function documentClick(e) {
     if(e.target.id != 'mydropdown')
         dropdownEl.classList.remove('active');
+}
+
+function burguerToggleActive() {
+    const burguerDiv = document.getElementById('burguerToggleDiv');
+    burguerDiv.classList.toggle('active');
+    const burguerMenu = document.getElementById('burguerMenu');
+    burguerMenu.classList.toggle('active');
 }
