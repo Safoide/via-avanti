ba034f5a18ab19d73931c281b2c0c6bf3e9f9f2a
Safoide <romanoibarra12@gmail.com>
2021-10-21 21:47:52 -0300

    position sticky agregado al menu hamburguesa

--- js/header.js
+++ js/header.js
@@ -1,5 +1,7 @@
 const dropdownEl = document.getElementById('mydropdown');
 const burguerEl = document.getElementById('burguerToggle');
+const burguerMenu = document.getElementById('burguerMenu');
+let active = false;
 
 dropdownEl.addEventListener('click', toggleActive);
 document.addEventListener('click', documentClick);
@@ -12,11 +14,24 @@ function toggleActive() {
 function documentClick(e) {
     if(e.target.id != 'mydropdown')
         dropdownEl.classList.remove('active');
+
+    if(e.target.id != 'burguerToggle' && e.target.id != 'burguerMenu') {
+        burguerMenu.classList.remove('active');
+        burguerEl.classList.remove('bxs-x-circle');
+        burguerEl.classList.add('bx-menu');
+        active = false;
+    }
 }
 
 function burguerToggleActive() {
-    const burguerDiv = document.getElementById('burguerToggleDiv');
-    burguerDiv.classList.toggle('active');
-    const burguerMenu = document.getElementById('burguerMenu');
+    !active ? (
+        burguerEl.classList.remove('bx-menu'),
+        burguerEl.classList.add('bxs-x-circle'),
+        active = true
+    ) : (
+        burguerEl.classList.add('bx-menu'),
+        burguerEl.classList.remove('bxs-x-circle'),
+        active = false
+    );
     burguerMenu.classList.toggle('active');
 }
