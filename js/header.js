96a3ac88516c05cbb2408c3fed782723b93034ae
Safoide <romanoibarra12@gmail.com>
2021-10-21 23:43:07 -0300

    Cambios en animacion

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 const dropdownEl = document.getElementById('mydropdown');
 const burguerEl = document.getElementById('burguerToggle');
 const burguerMenu = document.getElementById('burguerMenu');
+const burguerElDiv = document.getElementById('burguerToggleDiv');
 let active = false;
 
 dropdownEl.addEventListener('click', toggleActive);
@@ -17,21 +18,31 @@ function documentClick(e) {
 
     if(e.target.id != 'burguerToggle' && e.target.id != 'burguerMenu') {
         burguerMenu.classList.remove('active');
-        burguerEl.classList.remove('bxs-x-circle');
-        burguerEl.classList.add('bx-menu');
+        burguerElDiv.classList.remove('active');
+
+        setTimeout(() => {
+            burguerEl.classList.remove('bxs-x-circle');
+            burguerEl.classList.add('bx-menu');
+        }, 200);
+
         active = false;
     }
 }
 
 function burguerToggleActive() {
-    !active ? (
-        burguerEl.classList.remove('bx-menu'),
-        burguerEl.classList.add('bxs-x-circle'),
-        active = true
-    ) : (
-        burguerEl.classList.add('bx-menu'),
-        burguerEl.classList.remove('bxs-x-circle'),
-        active = false
-    );
+    burguerElDiv.classList.toggle('active');
+
+    setTimeout(() => {
+        !active ? (
+            burguerEl.classList.remove('bx-menu'),
+            burguerEl.classList.add('bxs-x-circle'),
+            active = true
+        ) : (
+            burguerEl.classList.add('bx-menu'),
+            burguerEl.classList.remove('bxs-x-circle'),
+            active = false
+        );
+    }, 200);
+   
     burguerMenu.classList.toggle('active');
 }
