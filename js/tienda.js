b3fd0a45122294d4b3f782c1561370a09996deab
Romano Ibarra <romanoibarra12@gmail.com>
2022-01-13 05:39:59 -0300

    Mas productos agregados al json, y cambios en script de tienda

--- js/tienda.js
+++ js/tienda.js
@@ -1,103 +1,131 @@
 const APIURL = "./data/data.json";
 
 let clicks = 0;
+let productsTotal = 0;
+let productsNow = 0;
 
 $.ajax({
     method: 'GET',
     url: APIURL,
     success: (response) => {
-        for(const product of response) {
+        response = response.filter(p => p.imagenes != undefined);
+
+        productsTotal = response.length;
+
+        for(let i = productsNow; i < productsNow + 16; i++) {
+            const product = response[i];
 
             const productHTML = document.createElement('li');
             productHTML.classList.add('lista__item');
 
-            product.preciodescuento ? (
+            product.precio_rebajado ? (
                 productHTML.innerHTML = `
-                    <a class="item__link descuento" href="./producto/${product.tag}.html">
-                        <img class="item__link--img fimg" src="${product.principal}" alt="FOTO DEL PRODUCTO">
-                        <img class="item__link--img simg" src="${product.secundaria}" alt="FOTO DEL PRODUCTO">
+                    <a class="item__link descuento" data-producto="${product.id}">
+                        <img class="item__link--img fimg" src="${product.imagenes.split(', ')[0]}" alt="FOTO DEL PRODUCTO">
+                        <img class="item__link--img simg" src="${product.imagenes.split(', ')[1]}" alt="FOTO DEL PRODUCTO">
                         <h3 class="item__link--title">${product.nombre.toUpperCase()}</h3>
                         <div class="link__precio">
-                            <span class="link__precio--precio descuento">$${product.precio}</span>
-                            <span class="link__precio--precio">$${product.preciodescuento}</span>
+                            <span class="link__precio--precio descuento">$${product.precio_normal}</span>
+                            <span class="link__precio--precio">$${product.precio_rebajado}</span>
                             <p class="link__precio--iva">- IVA Incluido</p>
                         </div>
                     </a>
                 `
             ) : (
                 productHTML.innerHTML = `
-                    <a class="item__link" href="./producto/${product.tag}.html">
-                        <img class="item__link--img fimg" src="${product.principal}" alt="FOTO DEL PRODUCTO">
-                        <img class="item__link--img simg" src="${product.secundaria}" alt="FOTO DEL PRODUCTO">
+                    <a class="item__link" data-producto="${product.id}">
+                        <img class="item__link--img fimg" src="${product.imagenes.split(', ')[0]}" alt="FOTO DEL PRODUCTO">
+                        <img class="item__link--img simg" src="${product.imagenes.split(', ')[1]}" alt="FOTO DEL PRODUCTO">
                         <h3 class="item__link--title">${product.nombre.toUpperCase()}</h3>
                         <div class="link__precio">
-                            <span class="link__precio--precio">$${product.precio}</span>
+                            <span class="link__precio--precio">$${product.precio_normal}</span>
                             <p class="link__precio--iva">- IVA Incluido</p>
                         </div>
                     </a>
                 `
             )
 
+            if (product.imagenes.split(', ')[1] == undefined) {
+                console.log(product);
+            }
+
             $('#productosUl').append(productHTML);
         }
+
+        productsNow += 16;
+
+        $(".item__link").click((e) => {
+            console.log(e.target.getAttribute('data-producto'));
+        })
     }
 })
 
 $('#cargarMas').click(function(){
-    if (clicks < 5)
+    if (clicks < productsTotal / 16)
         cargarMas();
 })
 
 function cargarMas() {
     clicks++;
     
-    for(let i = 0; i < 8; i++) {
-        
+    for(let i = productsNow; i < productsNow + 16; i++) {
         $.ajax({
             method: 'GET',
             url: APIURL,
             success: (response) => {
 
-                let randomItem = Math.floor(Math.random() * response.length);
+                response = response.filter(p => p.imagenes != undefined);
 
-                const product = response[randomItem];
+                if (i >= productsTotal) return $('#cargarMas').removeClass('show');
+
+                const product = response[i];
                 
                 const productHTML = document.createElement('li');
                 productHTML.classList.add('lista__item');
-    
-                product.preciodescuento ? (
+
+                product.precio_rebajado ? (
                     productHTML.innerHTML = `
-                        <a class="item__link descuento" href="./producto/${product.tag}.html">
-                            <img class="item__link--img fimg" src="${product.principal}" alt="FOTO DEL PRODUCTO">
-                            <img class="item__link--img simg" src="${product.secundaria}" alt="FOTO DEL PRODUCTO">
+                        <a class="item__link descuento" data-producto="${product.id}">
+                            <img class="item__link--img fimg" src="${product.imagenes.split(', ')[0]}" alt="FOTO DEL PRODUCTO">
+                            <img class="item__link--img simg" src="${product.imagenes.split(', ')[1]}" alt="FOTO DEL PRODUCTO">
                             <h3 class="item__link--title">${product.nombre.toUpperCase()}</h3>
                             <div class="link__precio">
-                                <span class="link__precio--precio descuento">$${product.precio}</span>
-                                <span class="link__precio--precio">$${product.preciodescuento}</span>
+                                <span class="link__precio--precio descuento">$${product.precio_normal}</span>
+                                <span class="link__precio--precio">$${product.precio_rebajado}</span>
                                 <p class="link__precio--iva">- IVA Incluido</p>
                             </div>
                         </a>
                     `
                 ) : (
                     productHTML.innerHTML = `
-                        <a class="item__link" href="./producto/${product.tag}.html">
-                            <img class="item__link--img fimg" src="${product.principal}" alt="FOTO DEL PRODUCTO">
-                            <img class="item__link--img simg" src="${product.secundaria}" alt="FOTO DEL PRODUCTO">
+                        <a class="item__link" data-producto="${product.id}">
+                            <img class="item__link--img fimg" src="${product.imagenes.split(', ')[0]}" alt="FOTO DEL PRODUCTO">
+                            <img class="item__link--img simg" src="${product.imagenes.split(', ')[1]}" alt="FOTO DEL PRODUCTO">
                             <h3 class="item__link--title">${product.nombre.toUpperCase()}</h3>
                             <div class="link__precio">
-                                <span class="link__precio--precio">$${product.precio}</span>
+                                <span class="link__precio--precio">$${product.precio_normal}</span>
                                 <p class="link__precio--iva">- IVA Incluido</p>
                             </div>
                         </a>
                     `
                 )
-    
+
+                if (product.imagenes.split(', ')[1] == undefined) {
+                    console.log(product);
+                }
+                
                 $('#productosUl').append(productHTML);
             }
         })
     }
 
-    if(clicks >= 5) {
+    productsNow += 16;
+
+    if(clicks >= productsTotal / 16) {
         $('#cargarMas').removeClass('show');
     }
+
+    $(".item__link").click((e) => {
+        console.log(e.target.getAttribute('data-producto'));
+    })
 }
